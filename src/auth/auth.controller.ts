c406b25dfc2e33345a53493feb52b9d8e1938f43
Alexis Gómez <00122717@uca.edu.sv>
2021-09-09 12:09:56 -0600

    :sparkles: Add sign out endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,18 @@
-import { Body, Controller, Get, Post, Query, Req } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Query,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './services/auth.service';
 import { SignUpDto } from './dtos/sign-up.dto';
 import { Request } from 'express';
 import { SignInDto } from './dtos/sign-in.dto';
+import { AuthGuard } from '@nestjs/passport';
+import { SignInGuard } from './guards/sign-in.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -21,5 +31,12 @@ export class AuthController {
   @Post('sign-in')
   signIn(@Body() signInDto: SignInDto) {
     return this.authService.signIn(signInDto);
+  }
+
+  @Get('sign-out')
+  @UseGuards(AuthGuard(), SignInGuard)
+  signOut(@Req() req: Request) {
+    console.log('signin out');
+    return this.authService.signOut(req);
   }
 }

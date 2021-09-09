559e609c05b907d61ee46a484c617b799b74e5b0
Alexis Gómez <00122717@uca.edu.sv>
2021-09-08 21:27:57 -0600

    :sparkles: Add sign-up endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, Req } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { SignUpDto } from './dtos/sign-up.dto';
+import { Request } from 'express';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('sign-up')
+  signUp(@Body() signUpDto: SignUpDto, @Req() req: Request) {
+    return this.authService.signUp(signUpDto, req);
+  }
+}

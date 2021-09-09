05cccad162dad45898b9ac9d7cab6d7985ad301b
Alexis Gómez <00122717@uca.edu.sv>
2021-09-09 09:46:59 -0600

    :sparkles: Add verify account endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,5 @@
-import { Body, Controller, Post, Req } from '@nestjs/common';
-import { AuthService } from './auth.service';
+import { Body, Controller, Get, Post, Query, Req } from '@nestjs/common';
+import { AuthService } from './services/auth.service';
 import { SignUpDto } from './dtos/sign-up.dto';
 import { Request } from 'express';
 
@@ -10,5 +10,10 @@ export class AuthController {
   @Post('sign-up')
   signUp(@Body() signUpDto: SignUpDto, @Req() req: Request) {
     return this.authService.signUp(signUpDto, req);
+  }
+
+  @Get('verify')
+  verifyAccount(@Query('user') userId: string) {
+    return this.authService.verifyAccount(userId);
   }
 }

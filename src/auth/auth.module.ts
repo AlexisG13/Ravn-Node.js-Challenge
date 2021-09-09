f9dc6dd00446381c599723d24ed69ecce524c81b
Alexis Gómez <00122717@uca.edu.sv>
2021-09-09 10:44:05 -0600

    :sparkles: Add jwt module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './services/auth.service';
+import { JwtStrategy } from './strategy/jwt.strategy';
 
 @Module({
   controllers: [AuthController],
-  providers: [AuthService],
-  imports: [PrismaModule],
+  providers: [AuthService, JwtStrategy],
+  imports: [
+    PrismaModule,
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigService],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: 3600 },
+      }),
+    }),
+  ],
 })
 export class AuthModule {}

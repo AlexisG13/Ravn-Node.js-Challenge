9034dd32b1b28011aac8861b43967d1719f50048
Alexis Gómez <00122717@uca.edu.sv>
2021-09-10 14:34:12 -0600

    :sparkles: Update imports and exports

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from 'src/auth/auth.module';
+import { CommentsModule } from 'src/comments/comments.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
+import { ReactionsModule } from 'src/reactions/reactions.module';
 import { PostsController } from './posts.controller';
 import { PostsService } from './posts.service';
 
 @Module({
   controllers: [PostsController],
   providers: [PostsService],
+  imports: [PrismaModule, ReactionsModule, CommentsModule, AuthModule],
+  exports: [PostsService],
 })
 export class PostsModule {}

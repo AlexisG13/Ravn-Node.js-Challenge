9802f711adf3f14348454a61d17fd5e82fa6490c
Alexis Gómez <00122717@uca.edu.sv>
2021-09-10 14:38:34 -0600

    :sparkles: Add get current user posts

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,4 +1,32 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Post as PostEntity, Reaction, User, Comment } from '@prisma/client';
+import { GetUser } from 'src/auth/decorators/get-user.decorator';
+import { CommentsService } from 'src/comments/comments.service';
+import { PostCommentDto } from 'src/comments/dtos/post-comment.dto';
+import { CreatePostDto } from './dtos/create-post.dto';
+import { ReactPostDto } from './dtos/react-post.dto';
+import { PostsService } from './posts.service';
 
 @Controller('posts')
-export class PostsController {}
+export class PostsController {
+  constructor(
+    private postService: PostsService,
+    private commentsService: CommentsService,
+  ) {}
+
+  @Get()
+  @UseGuards(AuthGuard())
+  getMyPosts(@GetUser() user: User): Promise<PostEntity[]> {
+    return this.postService.getUserPosts(user.id);
+  }
+}

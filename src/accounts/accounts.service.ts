3f08395cd3ab631f72a9dc17c6cddecb512fbef6
Alexis Gómez <00122717@uca.edu.sv>
2021-09-10 14:31:41 -0600

    :sparkles: Add get userInfo method

---
+++ src/accounts/accounts.service.ts
@@ -0,0 +1,27 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Profile, UserSettings } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
+import { UpdateSettingsDto } from './dtos/update-settings.dto';
+
+@Injectable()
+export class AccountsService {
+  constructor(private prisma: PrismaService) {}
+
+  async getUserInfo(userId: string) {
+    const userSettings = await this.prisma.userSettings.findUnique({
+      where: { userId },
+    });
+    const user = await this.prisma.user.findUnique({ where: { id: userId } });
+    if (!userSettings || !user) {
+      throw new NotFoundException('User does not exist');
+    }
+    if (userSettings.showEmail === false) {
+      user.email = null;
+    }
+    if (userSettings.showName === false) {
+      user.name = null;
+    }
+    return user;
+  }
+
+}

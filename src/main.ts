8067d78cc9702490b464d8950d9bc5a1b1fdea74
Alexis Gómez <00122717@uca.edu.sv>
2021-09-10 14:52:35 -0600

    :sparkles: Add swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ forbidUnknownValues: true }));
-  await app.listen(3000);
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('My U library')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();

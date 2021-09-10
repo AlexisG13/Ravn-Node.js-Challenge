582bd2af0f8a4f97063712bc14945c47dc91bc88
Alexis Gómez <00122717@uca.edu.sv>
2021-09-09 21:34:23 -0600

    :sparkles: Add getPost method

--- src/posts/posts.service.spec.ts
+++ src/posts/posts.service.spec.ts
@@ -1,18 +1,120 @@
+import {
+  BadRequestException,
+  ConflictException,
+  NotFoundException,
+} from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from '../prisma/prisma.service';
 import { PostsService } from './posts.service';
+
+const mockPrismaService = () => ({
+  post: {
+    findUnique: jest.fn(),
+    deleteMany: jest.fn(),
+    update: jest.fn(),
+    findMany: jest.fn(),
+  },
+  reactionReference: { findUnique: jest.fn() },
+  reaction: { create: jest.fn() },
+});
+
+const mockSinglePost = {
+  id: '789',
+  title: 'My test post',
+  content: 'Such test much wow',
+  authorId: '456',
+  isLive: true,
+  reactable: {
+    resourceId: '789',
+    resourceType: '1',
+  },
+};
+
+const mockFindMany = {
+  posts: [mockSinglePost],
+  get count() {
+    return this.posts.length;
+  },
+};
+
+const mockUpdatedPost = {
+  id: '9be9c1c6-731c-4ff3-ac58-a60b59b0a900',
+  title: 'My new test post',
+  content: 'Such test much wow',
+  authorId: '456',
+  isLive: true,
+};
+
+const updatePostDto = {
+  title: 'My new test post',
+  content: 'Such test much wow',
+  isDraft: false,
+};
+
+const failUpdatePostDto = {
+  title: 'My test post',
+  content: 'Such test much wow',
+  isDraft: true,
+};
+
+const mockDeleteMany = {
+  deletedElements: [mockSinglePost],
+  get count() {
+    return this.deletedElements.length;
+  },
+};
+const mockDeleteManyFail = {
+  deletedElements: [],
+  get count() {
+    return this.deletedElements.length;
+  },
+};
+
+const mockReactionReference = {
+  id: 1,
+  name: 'like',
+};
+
+const mockReaction = {
+  userId: '123',
+  postId: '789',
+  reactableId: '7891',
+};
 
 describe('PostsService', () => {
   let service: PostsService;
-
+  let prisma: PrismaService;
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PostsService],
+      providers: [
+        PostsService,
+        {
+          provide: PrismaService,
+          useFactory: mockPrismaService,
+        },
+      ],
     }).compile();
 
     service = module.get<PostsService>(PostsService);
+    prisma = module.get<PrismaService>(PrismaService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+    expect(prisma).toBeDefined();
+  });
+
+  describe('getPost', () => {
+    it('should get a post with the provided id', async () => {
+      (prisma.post.findUnique as jest.Mock).mockResolvedValue(mockSinglePost);
+      const result = await service.getPost('123');
+      expect(result).toEqual(mockSinglePost);
+    });
+
+    it('should throw not found exception when the post was not found', async () => {
+      (prisma.post.findUnique as jest.Mock).mockResolvedValue(null);
+      expect(service.getPost('456')).rejects.toThrowError(NotFoundException);
+    });
+  });
   });
 });
--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,4 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+import { Post } from '@prisma/client';
+import { PrismaService } from '../prisma/prisma.service';
+import { CreatePostDto } from './dtos/create-post.dto';
+import { ReactPostDto } from './dtos/react-post.dto';
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  constructor(private prisma: PrismaService) {}
+
+  async getPost(postId: string): Promise<Post> {
+    const post = await this.prisma.post.findUnique({ where: { id: postId } });
+    if (!post) {
+      throw new NotFoundException('Post not found');
+    }
+    return post;
+  }
+
+}

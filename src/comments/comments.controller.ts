05e93ac03bb13434f82dcb76b140d5d6c6c75013
Alexis Gómez <00122717@uca.edu.sv>
2021-09-10 14:42:04 -0600

    :sparkles: Add delete comment endpoint

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Reaction, User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorators/get-user.decorator';
+import { CommentsService } from './comments.service';
+import { PostCommentDto } from './dtos/post-comment.dto';
+import { ReactCommentDto } from './dtos/react-comment.dto';
 
 @Controller('comments')
-export class CommentsController {}
+export class CommentsController {
+  constructor(private commentsService: CommentsService) {}
+
+  @Delete('/:commentId')
+  @UseGuards(AuthGuard())
+  deleteComment(@Param('commentId') commentId: string, @GetUser() user: User) {
+    return this.commentsService.deleteComment(commentId, user.id);
+  }
+
+}
